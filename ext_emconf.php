1d7461c8fa008b4ff50df03f35a4e87d89dcba39
steffenk <steffenk@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-11-26 22:49:49 +0000

    renamed extension to beuser_iprange
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/user_iprange/trunk@14329 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "beuser_iprange"
+#
+# Auto generated 26-11-2008 23:45
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'IP-range for Admins/Be-user',
+	'description' => 'restrict BE-user to IP-range',
+	'category' => 'services',
+	'author' => 'Steffen Kamper',
+	'author_email' => 'info@sk-typo3.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.2-0.0.0',
+			'typo3' => '4.2-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"a712";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"6454";s:17:"ext_localconf.php";s:4:"92f1";s:14:"doc/manual.sxw";s:4:"90d6";s:34:"sv1/class.tx_beuseriprange_sv1.php";s:4:"ef29";}',
+	'suggests' => array(
+	),
+);
+
+?>
